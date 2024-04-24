d04ecb58449dec3af4ba53c2c05c6c85ec5a8c93
Anika Misra <amisra@cs.ucsb.edu>
2024-04-24 15:23:16 -0700

    am - define ENDPOINT for location code service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
@@ -4,5 +4,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocationController {
-    
+    public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
 }
